a80ff014af6418d417f57703d89c3c8b715e52d1
Prince E <109649366+peeebamsc@users.noreply.github.com>
2022-11-07 06:44:52 -0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        // OTP = One-Time pad
+        /*
+
+        encryption technique that cannot be cracked,
+        but require the use of a one-time pre-shared
+        key the same size as, or longer than, the message
+        being sent. In this technique, plaintext us paired
+        with a random secret key ( also referred to as one-time pad).
+
+         */
+
+    }
+}
